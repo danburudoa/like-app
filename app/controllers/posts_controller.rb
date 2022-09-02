ba364430509e4bccea11d1fad812799e386a6b3d
danburudoa <yumamukai515@gmail.com>
2022-09-02 17:26:06 +0900

    postsコントローラー実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,28 @@
 class PostsController < ApplicationController
+  
+  before_action :authenticate_user!, only: [:create, :show]
+
   def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  root "posts/index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   resources :users, only: [:index, :show]
-
+  
   resources :posts, only: [:index, :create, :show]
-
+  
+  root 'posts#index'
 end
