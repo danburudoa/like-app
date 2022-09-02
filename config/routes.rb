9198fe419f6b23462c306dfa63f7e958620933ea
danburudoa <yumamukai515@gmail.com>
2022-09-02 17:05:54 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'users/index'
-  get 'users/show'
+
+  root "posts/index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show]
+
+  resources :posts, only: [:index, :create, :show]
+
 end
